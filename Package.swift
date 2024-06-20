d439b1b1ed830e2f6573ffdc5385f1fa71c216c1
NareshBojja03 <naresh.bojja@valuelabs.com>
2024-06-20 20:48:10 +0530

    updated file location

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "NYT360Video",
+    products: [
+        .library(
+            name: "NYT360Video",
+            targets: ["NYT360Video"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "NYT360Video",
+            dependencies: [],
+            path: "Sources/NYT360Video",
+            publicHeadersPath: "include"
+        ),
+        .testTarget(
+            name: "NYT360VideoTests",
+            dependencies: ["NYT360Video"],
+            path: "Tests/NYT360VideoTests"),
+    ]
+)
